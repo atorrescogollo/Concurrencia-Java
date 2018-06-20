5a5f8373d166f5776ff4b6dc88fe7b665bac5973
Alvaro Torres Cogollo <atorrescogollo@gmail.com>
2018-06-20 23:33:41 +0200

    F3E2 Semaforo como control de paso

--- src/Hilo.java
+++ src/Hilo.java
@@ -1,34 +1,24 @@
-import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 public class Hilo implements Runnable {
 
 	private int id;
-	private ArrayList<String> list;
 	private Semaphore semaphore;
 
-	public Hilo(int id, ArrayList<String> list, Semaphore semaphore) {
+	public Hilo(int id, Semaphore semaphore) {
 		this.id=id;
-		this.list=list;
 		this.semaphore=semaphore;
 	}
 
 	@Override
 	public void run() {
-		System.out.println("[Hilo-"+id+"]: id="+id);
-
-		for (int i = 0; i < 10000; i++) {
-			try {
-				semaphore.acquire();
-				list.add("Texto");
-				semaphore.release();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		try {
+			System.out.println("[Hilo-"+id+"]: id="+id);
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}finally {
+			semaphore.release();
 		}
-
-		System.out.println("[Hilo-"+id+"]: numElementos="+list.size());
-
 	}
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 public class Main {
-	public static final int NUM_HILOS = 8;
+	public static  final int NUM_HILOS=10;
+	public static final int MAX_HILOS_PARALELO = 2;
+
 	private static ArrayList<Thread> hilos;
 
 	public static void main(String[] args) {
-		hilos = new ArrayList<>();
-		ArrayList<String> list = new ArrayList<>();
-
-		Semaphore semaphore = new Semaphore(1);
+		Semaphore semaphore = new Semaphore(MAX_HILOS_PARALELO);
+		hilos=new ArrayList<>();
 		for (int i = 0; i < NUM_HILOS; i++) {
-			Thread h = new Thread(new Hilo(i+1, list, semaphore), "Hilo-"+(i+1));
-			hilos.add(h);
-			h.start();
+			try {
+				Thread h = new Thread(new Hilo(i+1, semaphore), "Hilo-"+(i+1));
+				hilos.add(h);
+				semaphore.acquire();
+				h.start();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 
 		for (Thread h : hilos) {
@@ -24,7 +29,6 @@ public class Main {
 			}
 		}
 
-		System.out.println("[Hilo-principal]: La lista contiene "+list.size()+" elementos.");
+		System.out.println("[Hilo-principal]: Terminado!");
 	}
-
 }
